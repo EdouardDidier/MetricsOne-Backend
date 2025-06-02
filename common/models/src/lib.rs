a698dd7457a2e4c976af1cbc199a41b18b2a3b11
Edouard D. <zeddydy@hotmail.com>
2025-06-02 20:09:20 +0200

    Rename API models

--- common/models/src/lib.rs
+++ common/models/src/lib.rs
@@ -1,14 +1,14 @@
-pub mod drivers;
+pub mod driver;
 pub mod images;
-pub mod meetings;
-pub mod sessions;
-pub mod teams;
+pub mod meeting;
+pub mod session;
+pub mod team;
 
-pub use drivers::*;
+pub use driver::*;
 pub use images::*;
-pub use meetings::*;
-pub use sessions::*;
-pub use teams::*;
+pub use meeting::*;
+pub use session::*;
+pub use team::*;
 
 pub trait QueryParams<'q> {
     fn get_expands(&'q self) -> Vec<&'q str>;
